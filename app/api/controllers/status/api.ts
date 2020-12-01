47212e0d90ef0ab322ee1aa71231b381ca56fa5b
Julio Augusto <julioams@icloud.com>
2020-12-01 19:06:08 -0300

    refactor: add request parameter on function

--- app/api/controllers/status/api.ts
+++ app/api/controllers/status/api.ts
@@ -1,6 +1,6 @@
-import { Response } from 'express'
+import { Request, Response } from 'express'
 
-export default (res: Response) => {
+export default (req: Request, res: Response) => {
   return res.status(200).json({
     status: 200,
     message: 'API Online!',
